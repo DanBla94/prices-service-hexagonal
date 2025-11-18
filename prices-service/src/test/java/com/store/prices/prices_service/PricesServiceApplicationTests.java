2f5d8971ef0b3e35471de35240881ad546c28fdb
DBLGI7F <dblazquez@paradigmadigital.com>
2025-11-18 10:39:10 +0100

    initial project structure. Spring boot 3 and java 21

---
+++ prices-service/src/test/java/com/store/prices/prices_service/PricesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.prices.prices_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
